54c94504a3d4d9efe052973feeea29df9d87a79b
Lara Martin <larikimc@gmail.com>
2017-02-12 13:04:33 +0100

    set up RecyclerView

--- app/src/main/java/eu/laramartin/holidaysinvr/JapanFragment.java
+++ app/src/main/java/eu/laramartin/holidaysinvr/JapanFragment.java
@@ -3,6 +3,8 @@ package eu.laramartin.holidaysinvr;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +12,19 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by lara on 12.02.17.
  */
 public class JapanFragment extends Fragment {
+
+    @BindView(R.id.recycler)
+    RecyclerView recyclerView;
+    private Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,9 +34,17 @@ public class JapanFragment extends Fragment {
 
         VrItemAdapter adapter = new VrItemAdapter(getActivity(), list);
         View view = inflater.inflate(R.layout.fragment_vr_list, container, false);
-//        ListView listView = (ListView) view.findViewById(R.id.listView);
-//        listView.setAdapter(adapter);
-
+        unbinder = ButterKnife.bind(this, view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        unbinder.unbind();
+        super.onDestroyView();
     }
 }
--- app/src/main/java/eu/laramartin/holidaysinvr/VrItemAdapter.java
+++ app/src/main/java/eu/laramartin/holidaysinvr/VrItemAdapter.java
@@ -1,13 +1,30 @@
 package eu.laramartin.holidaysinvr;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 import java.util.List;
 
 /**
  * Created by lara on 12.02.17.
  */
-public class VrItemAdapter {
+public class VrItemAdapter extends RecyclerView.Adapter {
     public VrItemAdapter(Context context, List<VrItem> list) {
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
