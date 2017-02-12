13bdfc6c1ab97e755af7829d1cce3cc7444d0ca2
Lara Martin <larikimc@gmail.com>
2017-02-12 12:47:34 +0100

    properties for the vr item class

--- app/src/main/java/eu/laramartin/holidaysinvr/VrItem.java
+++ app/src/main/java/eu/laramartin/holidaysinvr/VrItem.java
@@ -1,7 +1,45 @@
 package eu.laramartin.holidaysinvr;
 
+import android.support.annotation.RawRes;
+import android.support.annotation.StringRes;
+
+import com.google.auto.value.AutoValue;
+
 /**
  * Created by lara on 12.02.17.
  */
-public class VrItem {
+
+@AutoValue
+public abstract class VrItem {
+    @StringRes
+    public abstract int getTitle();
+
+    public abstract int getType();
+
+    @StringRes
+    public abstract int getDescription();
+
+    @RawRes
+    public abstract int getResId();
+
+    public abstract String getAssetName();
+
+    public static Builder builder() {
+        return new AutoValue_VrItem.Builder();
+    }
+    
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder setTitle(int newTitle);
+
+        public abstract Builder setType(int newType);
+
+        public abstract Builder setDescription(int newDescription);
+
+        public abstract Builder setResId(int newResId);
+
+        public abstract Builder setAssetName(String newAssetName);
+
+        public abstract VrItem build();
+    }
 }
